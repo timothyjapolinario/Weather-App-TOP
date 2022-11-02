210affb2c4bf3ec807d5194a0cfbb76c82094e9f
JayJay <timothyjapolinario@gmail.com>
2022-11-02 15:12:55 +0800

    Fix negative time bug

--- src/modules/util/Time.js
+++ src/modules/util/Time.js
@@ -7,7 +7,11 @@ const getCurrentTimeFromUTC = function subtractTimeZoneFromUTC(timeZone) {
   const utc = getUTCTimeNow();
   const utcHour = parseInt(utc.slice(0, 2));
   const timeZoneHour = parseInt(timeZone.slice(0, 3));
-  const currentTime = utcHour + timeZoneHour;
+  let currentTime = utcHour + timeZoneHour;
+  if (currentTime < 0) {
+    currentTime *= -1;
+  }
+
   return currentTime + utc.slice(2);
 };
 
